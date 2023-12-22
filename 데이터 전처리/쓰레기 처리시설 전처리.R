999b83a757acde0585a79d2d9c1fd5e70de12c40
hinagumena <115136010+hinagumena@users.noreply.github.com>
2023-12-23 06:23:15 +0900

    Update 쓰레기 처리시설 전처리.R

--- 데이터 전처리/쓰레기 처리시설 전처리.R
+++ 데이터 전처리/쓰레기 처리시설 전처리.R
@@ -2,8 +2,8 @@ library(tidyverse)
 library(data.table)
 library(magrittr)
 
-data1<-fread("C:/Users/shn20/Desktop/작업물/통계분석학회/여름스터디/전처리/쓰레기 처리 시설.csv")
-data2<-fread("C:/Users/shn20/Desktop/작업물/통계분석학회/여름스터디/자료 다운로드/시군구위도경도.csv")
+data1<-fread("쓰레기 처리 시설.csv")
+data2<-fread("시군구위도경도.csv")
 
 view(data1);view(data2)
 
