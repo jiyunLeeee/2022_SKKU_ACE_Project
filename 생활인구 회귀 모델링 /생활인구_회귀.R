dc89f43334d13717ba1dc5ab68763fc6380ed72d
hinagumena <115136010+hinagumena@users.noreply.github.com>
2024-01-02 15:14:36 +0900

    Update 생활인구_회귀.R

--- 생활인구 회귀 모델링 /생활인구_회귀.R
+++ 생활인구 회귀 모델링 /생활인구_회귀.R
@@ -56,3 +56,43 @@ nor_minmax = function(x){
 y<-xy$총생활인구수 
 xy %<>% select(-총생활인구수) %>% apply(nor_minmax,2)
 xy<-cbind(xy,y) %>% rename('총생활인구수'='y')
+
+fit2<-lm(data=xy,총생활인구수~.)
+summary(fit2)
+vif(fit2)
+
+#xy %<>% na.omit
+xy_cor = cor(xy)
+ggcorrplot(xy_cor,type="lower", lab=T,lab_size=3)+theme_classic()+labs(title="상관관계")+
+  theme(axis.text.x = element_text(angle=45))
+
+null<-lm(data=xy,총생활인구수~1)
+full<-lm(data=xy,총생활인구수~.)
+step_result2<-step(null,scope=list(lower=null,upper=full),direction='both')
+
+step_result2
+
+stepfit2<-lm(formula = 총생활인구수 ~ 편의점 + `2022년06월_총인구수` + 
+    `토지 매매가` + 관광명소 + 카페, data = xy)
+summary(stepfit2)
+
+plot(stepfit2)
+gvlma(stepfit2)
+shapiro.test(stepfit2$residuals)
+ncvTest(stepfit2)
+
+library(caret)
+imp<-varImp(fit3)
+
+feature<-rownames(imp)
+score<-imp$Overall
+imp<-cbind(feature,score) %>% as.data.frame
+imp$importance<-score/sum(score)
+
+library(ggplot2)
+
+ggplot(imp,aes(x=reorder(feature,importance),y=importance))+
+  geom_bar(stat='identity',fill='blue')+coord_flip()+
+  xlab("feature")+theme_classic()+
+  ggtitle("Feature Importance")+
+  theme(plot.title=element_text(size=20,hjust=0.5),legend.position='none')
